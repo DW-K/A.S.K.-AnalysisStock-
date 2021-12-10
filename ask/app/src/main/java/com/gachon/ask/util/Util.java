885d42d17e9e1322b6c7800ff31fd6ae23dcaac4
Taehyuny <hyuny6012@naver.com>
2021-12-11 02:50:29 +0900

    Mock investment data(userMoney,userStock) ver.1

--- ask/app/src/main/java/com/gachon/ask/util/Util.java
+++ ask/app/src/main/java/com/gachon/ask/util/Util.java
@@ -1,7 +1,20 @@
 package com.gachon.ask.util;
 
+import java.text.DecimalFormat;
+
 public class Util {
     // Google Login Request Code
     public static final int RC_SIGN_IN = 1000;
+
+
+    /**
+     * 숫자에 천단위마다 콤마 넣기
+     * @param num
+     * @return String
+     * */
+    public static String toNumFormat(int num) {
+        DecimalFormat df = new DecimalFormat("#,###");
+        return df.format(num);
+    }
 
 }
