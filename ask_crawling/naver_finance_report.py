05cfe7af2e42c2c6847ba13b17939a98282aa780
Sojeong-Kim0915 <70928408+Sojeong-Kim0915@users.noreply.github.com>
2021-12-11 11:51:42 +0900

    네이버금융 재무제표 크롤링_연간, 분기

--- ask_crawling/naver_finance_report.py
+++ ask_crawling/naver_finance_report.py
@@ -4,43 +4,43 @@ import openpyxl
 
 # 한글깨짐 방지
 import sys
-import io
 
-import Path
-from Path import writeToExcel
-
-from sentiment import sentiment
 
 
 code = input("코드를 입력하세요 : ")
+companyName = input("기업이름을 입력하세요 : ")
 URL = f"https://finance.naver.com/item/main.nhn?code={code}"
 
-samsung_electronic = requests.get(URL)
-html = samsung_electronic.text
 
-financial_stmt = pd.read_html(samsung_electronic.text)[3]
+company_report= requests.get(URL)
+html = company_report.text
+
+financial_stmt = pd.read_html(company_report.text)[3]
 
 financial_stmt.set_index(('주요재무정보', '주요재무정보', '주요재무정보'), inplace=True)
 financial_stmt.index.rename('주요재무정보', inplace=True)
 financial_stmt.columns = financial_stmt.columns.droplevel(2)
-print(financial_stmt)
-data = financial_stmt
-data.to_excel(f'{code}.xlsx')
+annual_date = pd.DataFrame(financial_stmt).xs('최근 연간 실적', axis = 1)
+quarter_date = pd.DataFrame(financial_stmt).xs('최근 연간 실적', axis = 1)
+
+annual = annual_date
+annual.to_excel(f'{companyName}_{code}_annual.xlsx')
+
+quarter = quarter_date
+quarter.to_excel(f'{companyName}_{code}_quarter.xlsx')
 
 
 
-if __name__ == "__main__":
-    arg_list = sys.argv[1:]  # argument 받아서 실행
-    category = arg_list[0]
-    companyName = arg_list[1]
-    query = arg_list[3]
-    sort = arg_list[4]
+
+#
+# if __name__ == "__main__":
+#     arg_list = sys.argv[1:]
+#     category = arg_list[0]
+#     companyName = arg_list[1]
+#     query = arg_list[3]
+#
+#
 
 
-    # filePath = crawler(category="에스엠", maxpage=20, query="이수만", sort="0", s_date="20211024", e_date="20201025")
-
-    filePath, output_file_name = crawler(category=category, companyName=companyName, maxpage=query, sort=sort)
-
-    sentiment(filePath=filePath, output_file_name=output_file_name, sheetName=query)
 
 
