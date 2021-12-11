94963db0b065184ef8ce6fa989a9e15b1eb8f8e0
Sojeong-Kim0915 <70928408+Sojeong-Kim0915@users.noreply.github.com>
2021-12-11 09:44:06 +0900

    네이버금융 재무제표 크롤링

--- ask_crawling/naver_finance_report.py
+++ ask_crawling/naver_finance_report.py
@@ -2,6 +2,16 @@ import pandas as pd
 import requests
 import openpyxl
 
+# 한글깨짐 방지
+import sys
+import io
+
+import Path
+from Path import writeToExcel
+
+from sentiment import sentiment
+
+
 code = input("코드를 입력하세요 : ")
 URL = f"https://finance.naver.com/item/main.nhn?code={code}"
 
@@ -16,3 +26,21 @@ financial_stmt.columns = financial_stmt.columns.droplevel(2)
 print(financial_stmt)
 data = financial_stmt
 data.to_excel(f'{code}.xlsx')
+
+
+
+if __name__ == "__main__":
+    arg_list = sys.argv[1:]  # argument 받아서 실행
+    category = arg_list[0]
+    companyName = arg_list[1]
+    query = arg_list[3]
+    sort = arg_list[4]
+
+
+    # filePath = crawler(category="에스엠", maxpage=20, query="이수만", sort="0", s_date="20211024", e_date="20201025")
+
+    filePath, output_file_name = crawler(category=category, companyName=companyName, maxpage=query, sort=sort)
+
+    sentiment(filePath=filePath, output_file_name=output_file_name, sheetName=query)
+
+
