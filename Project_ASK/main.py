3a4e80726d87bc5ceb2796f4304a6dec5c6b55b5
DW-K <pch145@naver.com>
2021-12-12 18:45:31 +0900

    fix gitignore

--- Project_ASK/main.py
+++ Project_ASK/main.py
@@ -55,16 +55,18 @@ if __name__ == "__main__":
     # crawlingStock()
     # newsCrawlingByKeyword()
     # twitterCrawlingByKeyword()
-
-    date_count = 50
-    for i in range(0, date_count, 1):
-        print(f'{i}/{date_count}')
-        target_date_format = now - timedelta(days=date_count - i)
-        target_date = target_date_format.strftime(dateFormat)
-        newsCrawlingByKeyword(target_date)
+    #
+    # date_count = 50
+    # for i in range(0, date_count, 1):
+    #     print(f'{i}/{date_count}')
+    #     target_date_format = now - timedelta(days=date_count - i)
+    #     target_date = target_date_format.strftime(dateFormat)
+    #     newsCrawlingByKeyword(target_date)
 
     # date_count = 6
     # for i in range(0, date_count, 1):
     #     target_date_format = now - timedelta(days=date_count - i)
     #     target_date = target_date_format.strftime(dateFormat)
     #     twitterCrawlingByKeyword(target_date)
+
+    print("hi")
