9bd71a9e69d5642a4d120c4e52eb240fcec106bf
Mina <00minha@gmail.com>
2022-02-13 22:54:21 +0900

    Refactor Ranking function
    랭킹 변경
    - recyclerview -> swiperefreshlayout 기능 추가
    - rank 기준: 수익률/레벨/대학
    - 보여주는 data(item) 구성 변경
        - 기준에 의한 순위(viewholder position값)
        - 전날 대비 순위 변동 값(userLastRank - userRank 절댓값) + 증가/유지/감소 이미지
        - 프로필(userProfile, userLevel, userNickname)
        - 수익률(보유한 주식에 의한 전체 수익률(home 화면에서 보여주는 것과 동일한 값))

--- ask/app/src/main/java/com/gachon/ask/RankingAdapter.java
+++ ask/app/src/main/java/com/gachon/ask/RankingAdapter.java
@@ -1,80 +1,149 @@
 package com.gachon.ask;
 
+import static java.security.AccessController.getContext;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.rpc.context.AttributeContext;
 
-import java.text.DecimalFormat;
-import java.util.List;
+import java.util.ArrayList;
 
-public class RankingAdapter extends RecyclerView.Adapter<ViewHolderRanking> {
-    RankingFragment rankingFragment;
-    List<RankInfo> rankInfoList;
+public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHolder> {
+    ItemClickListener itemClickListener;
+    ArrayList<RankInfo> items = new ArrayList<RankInfo>();
+    private static final String TAG = "RankingAdapter";
+    private FirebaseUser user;
+    private Context context;
 
-    public RankingAdapter(RankingFragment rankingFragment, List<RankInfo> rankInfoList) {
-        this.rankingFragment = rankingFragment;
-        this.rankInfoList = rankInfoList;
+    public RankingAdapter(Context context) {
+        this.context = context;
     }
 
 
     @NonNull
     @Override
-    public ViewHolderRanking onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        //inflate layout
-        View itemView = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.ranking_view_layout, viewGroup,false);
-
-        ViewHolderRanking viewHolderRanking = new ViewHolderRanking(itemView);
-
-
-//        // item clicked
-//        viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-//            @Override
-//            public void onItemClick(View view, int position) {
-//
-//                Intent intent = new Intent(view.getContext(), PostViewActivity.class);
-//                intent.putExtra("rank",user.get(position).getNickname());
-//                intent.putExtra("level",user.get(position).getContents());
-//                intent.putExtra("nickname",user.get(position).getPublisher());
-//                intent.putExtra("university",user.get(position).getCategory());
-//                intent.putExtra("asset",user.get(position).getCreatedAt());
-//
-//
-//                categoryActivity.startActivity(intent);
-//
-//            }
-//
-//            @Override
-//            public void onItemLongClick(View view, int position) {
-//
-//            }
-//
-//        });
-
-
-        return viewHolderRanking;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.item_ranking, viewGroup, false);
+        return new ViewHolder(itemView);
     }
 
-    DecimalFormat myFormatter = new DecimalFormat("###,###");
-
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderRanking holder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        RankInfo item = items.get(position);
+        viewHolder.setItem(item);
 
 
-        //bind views
-        holder.vRank.setText(rankInfoList.get(i).getUserRank().toString() + " 위");
-        holder.vLevel.setText("Lv. " + rankInfoList.get(i).getUserLevel().toString());
-        holder.vNickname.setText(rankInfoList.get(i).getNickname());
-        holder.vAsset.setText(myFormatter.format(rankInfoList.get(i).getUserMoney()) + "원");
     }
 
     @Override
     public int getItemCount() {
-        return rankInfoList.size();
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_change, iv_profile, iv_univ;
+        TextView tv_rank, tv_rankChange, tv_level, tv_nickname, tv_yield;
+        int item_pos;
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            tv_rank = (TextView) itemView.findViewById(R.id.tv_rank);
+            iv_change = itemView.findViewById(R.id.iv_change);
+            iv_profile = itemView.findViewById(R.id.iv_profile);
+            tv_rankChange = (TextView) itemView.findViewById(R.id.tv_change);
+            tv_level = (TextView) itemView.findViewById(R.id.tv_level);
+            tv_nickname = (TextView) itemView.findViewById(R.id.tv_nickname);
+            tv_yield = (TextView) itemView.findViewById(R.id.tv_yield);
+
+            iv_univ= itemView.findViewById(R.id.iv_univ);
+            user = FirebaseAuth.getInstance().getCurrentUser();
+
+            item_pos = getAdapterPosition() + 1;
+
+            // 추후 click event (프로필 이동 등)
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext() , "click한 위치의 item_pos: "+item_pos , Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+
+
+        public void setItem(RankInfo item) {
+            int rank = item.getuNewRank();
+            int uRankChange = item.getuLastRank() - rank;
+
+            // 순위 변동에 따라 화살표 그림 바꾸기
+            int iv_rankChange_name;
+            if(uRankChange > 0) iv_rankChange_name = R.drawable.outline_arrow_drop_up_24;
+            else if(uRankChange < 0) iv_rankChange_name = R.drawable.outline_arrow_drop_down_24;
+            else iv_rankChange_name = R.drawable.outline_remove_24;
+
+            iv_change.setImageResource(iv_rankChange_name);
+
+            uRankChange = Math.abs(uRankChange);
+            tv_rank.setText(rank +" 위"); // 순위 ex) 1위
+            tv_rankChange.setText(String.valueOf(uRankChange)); // 변동 ex) 4
+            tv_level.setText(context.getResources().getString(R.string.level) + String.valueOf(item.getuLevel())); // 레벨 ex) Lv.3
+            tv_yield.setText(String.valueOf(item.getuYield())); // 수익률 ex) 32 %
+            tv_nickname.setText(String.valueOf(item.getuNickname())); // 유저닉네임 ex) 민하
+
+
+
+        }
     }
 
 
+    public void addItem(RankInfo item) {
+        items.add(item);
+    }
+
+    public void setItems(ArrayList<RankInfo> items) {
+        this.items = items;
+    }
+
+    public RankInfo getItem(int position) {
+        return items.get(position);
+    }
+
+    public void setItem(int position, RankInfo item) {
+        items.set(position, item);
+    }
+
+    public interface ItemClickListener
+    {
+        void onItemClick(int position);
+    }
+
+    public void StartToast(Integer msg){
+        Toast toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
+
